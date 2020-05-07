f93a0dfcb96623dee740f65a8a8b744454b374b6
lekdan49 <14988063+lekdan49@users.noreply.github.com>
2020-05-08 04:12:16 +1000

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # osu! pp graph
+Creates a small plot of your scores and pp over time.
+
+![sample image](https://raw.githubusercontent.com/lekdan49/osu-score-graph/master/sample_image.png)
+
+## Usage
+
+```python
+python run_me.py
+```
+
+
+
+## Requirements
+check requirements.txt
--- run_me.py
+++ run_me.py
@@ -5,10 +5,10 @@ import scraper
 
 def set_sizes():
     fig_size = plt.rcParams["figure.figsize"]
-    print("Current size:", fig_size)  # prints default res witch is 640 x 480
+    # print("Current size:", fig_size)  # prints default res witch is 640 x 480
     fig_size[0] = 20  # sets resolution on x axis to 2000
     fig_size[1] = 6.4  # sets resolution on y axis to 640
-    print("Current size:", fig_size)  # prints current resolution which is 2000 x 640
+    # print("Current size:", fig_size)  # prints current resolution which is 2000 x 640
 
     plt.tick_params(axis='x', which='major', labelsize=5)  # change x axis font
     plt.tick_params(axis='x', which='minor', labelsize=4)  # change x axis font
@@ -23,8 +23,10 @@ def plot_graph(pp1, score_date2, user_name1):
 
     plt.legend()  # actually print the legend
 
-    plt.savefig('pp_graph.png', dpi=300)  # saves pp graph as .png
-    plt.savefig("graph.svg")  # saves an svg
+    png_saved = user_name1[0] + '.png'
+    svg_saved = user_name1[0] + '.svg'
+    plt.savefig(png_saved, dpi=300)  # saves pp graph as .png
+    plt.savefig(svg_saved)  # saves an svg
     plt.show()
 
 
@@ -33,8 +35,8 @@ def main():
 
     pp1, score_date2 = zip(*sorted(zip(score_date2, pp1)))  # sorts dates and pp
 
-    print(pp1)
-    print(score_date2)
+    # print(pp1)
+    # print(score_date2)
 
     set_sizes()
     plot_graph(pp1, score_date2, user_name1)
