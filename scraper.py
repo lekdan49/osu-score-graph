4bca08380453196816ec00d6f57fd0f3fe0cf22d
lekdan49 <14988063+lekdan49@users.noreply.github.com>
2020-05-08 00:12:05 +1000

    Did a lot of stuff!

--- scraper.py
+++ scraper.py
@@ -1,39 +1,68 @@
+import glob
+import json
+
 import requests
 
 
 def get_URL(url):
-
-    r = requests.get(url)
+    r = requests.get(url).json()
 
     return r
 
 
 def get_osu_id():
+    # x = str(input("Please enter your Osu! account number "
+    #
+    #               "this is the number at the #'s "
+    #
+    #               "https://osu.ppy.sh/users/##### "))
 
-    x = str(input("Please enter your Osu! account number "
-                  
-                  "this is the number at the #'s "
-                  
-                  "https://osu.ppy.sh/users/##### "))
-
+    x = "7575434"
     return x
 
 
 def create_osu_url(osu_id):
-
     x = ("https://osu.ppy.sh/users/{}/scores/best?mode=osu&offset=0&limit=51".format(osu_id))
 
     return x
 
 
+def parse_json(file_name):
+    with open(file_name, "r") as content:
+        parsed_json = json.load(content)
+        for i in parsed_json:
+            print(i['beatmapset']['title'], i['created_at'])    # Keep in mind indentation in .json!
+
+
+def write_json_to_file(json_name, created_osu_url):
+    with open(json_name, "w") as json_output:  # When using 'with' we don't need to close the file! "
+
+        json.dump(get_URL(created_osu_url), json_output)
+
 
 def main():
+    osu_id = get_osu_id()
 
-    print(get_URL(create_osu_url(get_osu_id())).text)
+    created_osu_url = create_osu_url(osu_id)
 
+    print(created_osu_url)
 
+    json_name = osu_id + ".json"
+
+    if json_name in glob.glob("*.json"):
+
+        print("ID already queried, parsing json... ")
+
+        parse_json(json_name)
+
+    elif json_name not in glob.glob("*.json"):
+
+        print("new ID, getting scores...")
+
+        write_json_to_file(json_name, created_osu_url)
+
+    parse_json(json_name)
 
 
 if __name__ == '__main__':
-
     main()
